0f607f122f08f33675d3c8a734c130eb87bfbfa4
Ankit Solanki <ankit03june@gmail.com>
2020-08-05 13:30:01 +0530

    TRIE needs to take into account txnID and vout from input reference

--- scripts/src/utxo_trie.py
+++ scripts/src/utxo_trie.py
@@ -1,95 +1,97 @@
 class UTXOTrieNode:
     def __init__(self):
         self.children = {}
-        self.is_end_node = False
         self.end_list = []
 
     def __repr__(self):
-        print(self.children) 
-        print(self.end_list) 
-        return ""
+        s = ""
+        s += str(self.children) 
+        s += str(self.end_list) 
+        return s
 
 class UTXOTrie:
-    def __init__(self):
-        self.depth = 2
+    def __init__(self, depth=2):
+        self.depth = depth
         self.root_node = UTXOTrieNode()
 
-    def insert(self, txn):
-        self.__insert__(self.root_node, txn, 0)
+    def insert(self, txn, node=None, index=0):
+        if index == 0:
+            node = self.root_node
 
-    def __insert__(self, node, txn, index):
         if index == self.depth:
-            node.end_list.append(txn.txnid)
+            node.end_list.append((txn.txnid, txn.vout))
             return
 
         if txn.txnid[index] not in node.children:
             node.children[txn.txnid[index]] = UTXOTrieNode()
 
-        self.__insert__(node.children[txn.txnid[index]], txn, index+1)
-        return
+        self.insert(txn, node.children[txn.txnid[index]], index+1)
 
-    def search(self, txn):
-        return self.__search__(self.root_node, txn, 0)
+    def search(self, txn, node=None, index=0):
+        if index == 0:
+            node = self.root_node
 
-    def __search__(self, node, txn, index):
         if index == self.depth:
-            return True if txn.txnid in node.end_list else False
+            return True if (txn.txnid, txn.vout) in node.end_list else False
 
         if txn.txnid[index] not in node.children:
             return False
 
-        return self.__search__(node.children[txn.txnid[index]], txn, index+1)
+        return self.search(txn, node.children[txn.txnid[index]], index+1)
 
-    def remove(self, txn):
-        self.__remove__(self.root_node, txn, 0)
+    def remove(self, txn, node=None, index=0):
+        if index == 0:
+            node = self.root_node
 
-    def __remove__(self, node, txn, index):
         if index == self.depth:
-            if txn.txnid in node.end_list:
-                node.end_list.remove(txn.txnid)
+            if (txn.txnid, txn.vout) in node.end_list:
+                node.end_list.remove((txn.txnid, txn.vout))
             return
 
         if txn.txnid[index] not in node.children:
             return
 
-        self.__remove__(node.children[txn.txnid[index]], txn, index+1)
+        self.remove(txn, node.children[txn.txnid[index]], index+1)
 
-    def print(self):
-        self.__print__(self.root_node, 0)
+    def print(self, node=None, index=0):
+        if index == 0:
+            node = self.root_node
 
-    def __print__(self, node, index):
         if index == self.depth:
             print(node.end_list)
             return
 
         for key in node.children:
             print(key, end=" -> ")
-            self.__print__(node.children[key], index+1)
-
-        return
+            self.print(node.children[key], index+1)
 
 class T:
-    def __init__(self, t):
-        self.txnid = t 
+    def __init__(self, txnid, vout):
+        self.txnid = txnid
+        self.vout = vout 
 
 if __name__ == '__main__':
-
-    u = UTXOTrie()
-    u.insert(T("ab2356"))
-    u.insert(T("ac2356"))
-    u.insert(T("ac226D"))
-    u.insert(T("acx26D"))
-    u.insert(T("3c2356"))
-    u.insert(T("5c2356"))
-    u.insert(T("5a2346"))
+    # testing that if the data structure works
+    u = UTXOTrie(depth=2)
+    u.insert(T("ab2356", 0))
+    u.insert(T("ab2356", 1))
+    u.insert(T("ab2356", 2))
+    u.insert(T("ac2356", 2))
+    u.insert(T("ac226D", 0))
+    u.insert(T("acx26D", 2))
+    u.insert(T("3c2356", 3))
+    u.insert(T("5c2356", 2))
+    u.insert(T("5a2346", 1))
+    
     u.print()
-    print(u.search(T("5a2346")))
-    print(u.search(T("acx26D")))
-    print(u.search(T("xzx26D")))
 
-    u.remove(T("ac226D"))
-    u.remove(T("acx26D"))
-    u.remove(T("5c2356"))
-    u.remove(T("3c2356"))
-    u.remove(T("5a2346"))
+    print(u.search(T("5a2346", 1)))
+    print(u.search(T("acx26D", 2)))
+    print(u.search(T("xzx26D", 0)))
+
+    u.remove(T("ac226D", 0))
+    u.remove(T("ab2356", 2))
+    u.remove(T("5c2356", 2))
+    (u.remove(T("xzx26D", 0)))
+
     u.print()
