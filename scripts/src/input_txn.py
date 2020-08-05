c94764b9d0bcd3c8089b796023341c4ff05dc661
EnigmaBreaker <cs5160398@cse.iitd.ac.in>
2020-08-05 13:59:25 +0530

    Added transaction

--- scripts/src/input_txn.py
+++ scripts/src/input_txn.py
@@ -1,7 +1,20 @@
-class InputTXN:
-    def __init__(self):
-        self.txid = None
-        self.vout = None
-        self.signature_script = None
+from utils import reverse_bytes
 
-        
+class InputTXN:
+    def __init__(self, txid, vout, signature_script):
+        self.txid = txid
+        self.vout = vout
+        self.signature_script = signature_script
+
+    def get_txn_input_data(self):
+    	reverse_txid = reverse_bytes(self.txid)
+    	vout_to_hex = hex(self.vout)[2:]
+    	vout_to_hex = '0'*(8-len(vout_to_hex)) + vout_to_hex
+    	reverse_vout = reverse_bytes(vout_to_hex)
+    	scriptsig_size = hex(len(self.signature_script)//2)[2:]
+    	return reverse_txid + reverse_vout + scriptsig_size + self.signature_script + "ffffffff"
+
+
+if __name__ == "__main__":
+	inptxn = InputTXN("123abcd4092e",2, "aedfasdfsdfe")
+	print(inptxn.get_txn_input_data())
--- scripts/src/output_txn.py
+++ scripts/src/output_txn.py
@@ -1,4 +1,18 @@
+from utils import reverse_bytes
+
 class OutputTXN:
-    def __init__(self):
-        self.amount = None
-        self.script_pub_key = None 
+    def __init__(self, amount, script_pub_key):
+        self.amount = amount
+        self.script_pub_key = script_pub_key
+
+    def get_txn_output_data(self):
+    	hex_amount = hex(self.amount)[2:]
+    	hex_amount = (16 - len(hex_amount))*'0' + hex_amount
+    	reverse_amount = reverse_bytes(hex_amount)
+    	pubkey_size = hex(len(self.script_pub_key)//2)[2:]
+    	return reverse_amount + pubkey_size + self.script_pub_key
+
+
+if __name__ == "__main__":
+	output_txn = OutputTXN(314, "abcdef123456")
+	print(output_txn.get_txn_output_data())
